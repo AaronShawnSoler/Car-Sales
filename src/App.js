c6cf91974387bfe099a6893f677449fa25852411
Aaron Shawn Soler <aaronshawnsoler@gmail.com>
2019-12-10 23:20:44 -0500

    redux data flow fix

--- src/App.js
+++ src/App.js
@@ -6,37 +6,38 @@ import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
 
 import { connect } from 'react-redux';
-import { reducer, initialState } from './reducers/reducer';
+// import { reducer, initialState } from './reducers/reducer';
 import { BuyItemContext, RemoveFeatureContext } from './contexts/contexts';
 import { addItem, removeItem } from './actions/actions';
 
-const App = () => {
+const App = (props) => {
 
-  const [state, dispatch] = useReducer(reducer, initialState);
+  // const [state, dispatch] = useReducer(reducer, initialState);
 
   const removeFeature = item => {
     // dispatch an action here to remove an item
-    dispatch(removeItem(item));
+    props.removeItem(item);
   };
 
   const buyItem = item => {
     // dipsatch an action here to add an item
-    dispatch(addItem(item));
+    props.addItem(item);
   };
 
-  console.log('state', state);
+  // console.log('state', state);
+  console.log('props', props);
 
   return (
     <RemoveFeatureContext.Provider value={removeFeature}>
       <BuyItemContext.Provider value={buyItem}>
         <div className="boxes">
           <div className="box">
-            <Header car={state.car} />
-            <AddedFeatures car={state.car} />
+            <Header car={props.car} />
+            <AddedFeatures car={props.car} />
           </div>
           <div className="box">
-            <AdditionalFeatures additionalFeatures={state.additionalFeatures} />
-            <Total car={state.car} additionalPrice={state.additionalPrice} />
+            <AdditionalFeatures additionalFeatures={props.additionalFeatures} />
+            <Total car={props.car} additionalPrice={props.additionalPrice} />
           </div>
         </div>
       </BuyItemContext.Provider>
@@ -52,4 +53,4 @@ const mapStateToProps = state => {
   }
 }
 
-export default connect(mapStateToProps, {})(App);
+export default connect(mapStateToProps, {addItem, removeItem})(App);
